3715f77ca35bb671f761432f86496aa9c922018e
Aakash Chakravarty <112715657+aaka8566@users.noreply.github.com>
2023-06-13 14:46:08 +0530

    mock12

--- Routes/Posting.route.js
+++ Routes/Posting.route.js
@@ -10,12 +10,13 @@ postingRoute.get("/",async(req,res)=>{
     catch(err){res.status(400).send(err)}
     })
     postingRoute.get("/jobs",async(req,res)=>{
-        const filter={};const sort={};
+        const filter={};const sort={};const page=+(req.query.page); 
         if(req.query.Role){filter.Role={$eq:req.query.Role}}
         if(req.query.postedat){sort.postedat=req.query.postedat}
+        if(req.query.page){}
         console.log(req.query);
         try{
-            const jobs=await postingModel.find(filter).sort({postedat:sort.postedat});
+            const jobs=await postingModel.find(filter).skip((page-1)*10).limit(10).sort({postedat:sort.postedat});
             res.status(200).send(jobs);
         }
         catch(err){res.status(400).send(err)}

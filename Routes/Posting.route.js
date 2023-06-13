1864730620746df47c7270177f009fd06ef410c4
Aakash Chakravarty <112715657+aaka8566@users.noreply.github.com>
2023-06-13 13:04:19 +0530

    mock12

--- Routes/Posting.route.js
+++ Routes/Posting.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const postingRoute=express.Router();
 const {postingModel}=require("../Models/Posting.model.js");
-
-
+const cors=require("cors");
+postingRoute.use(cors());
 postingRoute.get("/",async(req,res)=>{
     try{
         res.status(200).send("job");
@@ -24,21 +24,4 @@ try{
 }
 catch(err){res.status(400).send(err)}
 });
-
-postingRoute.patch("/edit",async(req,res)=>{
-    try{
-        const job=await postingModel(req.body);
-        job.save();
-        res.status(200).send(job);
-    }
-    catch(err){res.status(400).send(err)}
-    });
-    postingRoute.delete("/delete",async(req,res)=>{
-        try{
-            const job=await postingModel(req.body);
-            job.save();
-            res.status(200).send(job);
-        }
-        catch(err){res.status(400).send(err)}
-        });
 module.exports={postingRoute};
